c077599a2c0663152a989fef6225509c2150aa32
FrankTehila <tf734169@gmail.com>
2024-12-13 10:24:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 // Enable CORS for all routes
 app.use(cors());
 
@@ -11,6 +10,10 @@ app.get('/api', (req, res) => {
     res.send({ message: 'Hello from backend!' });
 });
 
+app.get('/', (req, res) => { // For health check
+    res.status(200).json({ status: 'UP' });
+  });
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
